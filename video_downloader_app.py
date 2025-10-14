fd4355d90861176b3e63bcfb82acc2ff00b54cd6
DGA-Research <dgaresearchcoding@gmail.com>
2025-10-14 14:26:52 -0400

    Update video_downloader_app.py

--- video_downloader_app.py
+++ video_downloader_app.py
@@ -702,6 +702,11 @@ if yt_dlp_outdated:
     )
 
 st.caption("Known issues: Does not work with some reigon-gated YouTube videos")
+st.caption(
+    "Known issues:"
+    "- Does not work with some reigon-gated YouTube videos or livestreams\n"
+    "- Long videos and large batch downloads can cause crashes"
+            )
 if not FFMPEG_AVAILABLE:
     st.warning("ffmpeg not detected. Install ffmpeg to enable audio/video clipping and proper muxing.")
 
@@ -1023,4 +1028,5 @@ if st.session_state.pop("continue_requested", False):
 
 if processing_triggered:
     st.rerun()
+
 
